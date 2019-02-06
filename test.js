1e8e2a834ad2020ed6df400d78e18940065375ef
Jesse Mejia <jmejia@anestheticaudio.com>
2019-02-06 14:12:11 -0800

    texture_test

--- test.js
+++ test.js
@@ -1,16 +1,22 @@
-var xLoc = 0;
+// video source: https://vimeo.com/90312869
+let img;
+let theta = 0;
 
 function setup() {
-  createCanvas(500, 500);
-  frameRate(60);
+  createCanvas(710, 400, WEBGL);
 
+  img = loadImage('assets/cat.jpg');
 }
 
 function draw() {
-  background(100, 244,50);
-  rect(xLoc, mouseY, 80, 80);
-
+  background(250);
+  translate(440, 0, 0);
+  push();
+  rotateZ(theta * 0.1);
+  rotateX(theta * 0.1);
+  rotateY(theta * 0.1);
+  texture(img);
+  box(100, 100, 100);
+  pop();
+  theta += 0.05;
 }
-
-
-
